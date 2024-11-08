4af845cf5ca648c8b7da96b6a010dfdb3a81a1f0
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-11-08 13:32:30 -0800

    Update MQTTSubscriber.java

--- eyeSimulator/src/main/java/test/MQTTSubscriber.java
+++ eyeSimulator/src/main/java/test/MQTTSubscriber.java
@@ -1,45 +1,69 @@
 package test;
 
+import app.DataDestination;
 import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MQTTSubscriber implements MqttCallback {
-
+public class MQTTSubscriber implements Runnable, MqttCallback {
     private static final Logger logger = LoggerFactory.getLogger(MQTTSubscriber.class);
     private static final String BROKER_URL = "tcp://test.mosquitto.org:1883";
     private static final String TOPIC = "eye-tracking/topic";
-    private static final String CLIENT_ID = "EyeTrackingSubscriber";
+    private final DataDestination dataDestination;
+    private volatile boolean running = true;
+    private MqttClient client;
 
-    public MQTTSubscriber() {
+    public MQTTSubscriber(DataDestination destination) {
+        this.dataDestination = destination;
+    }
+
+    public void stopSubscriber() {
+        running = false;
+        if (client != null && client.isConnected()) {
+            try {
+                client.disconnect();
+                logger.info("Disconnected from MQTT broker.");
+            } catch (MqttException e) {
+                logger.error("Error while disconnecting MQTTSubscriber", e);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
         try {
-            MqttClient client = new MqttClient(BROKER_URL, CLIENT_ID);
+            client = new MqttClient(BROKER_URL, MqttClient.generateClientId());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
             client.setCallback(this);
-            client.connect();
-            logger.info("Connected to broker: {}", BROKER_URL);
+            client.connect(options);
             client.subscribe(TOPIC);
-            logger.info("Subscribed to topic: {}", TOPIC);
-        } catch (MqttException e) {
-            logger.error("Failed to connect or subscribe to MQTT Broker", e);
+            logger.info("Connected to MQTT broker and subscribed to topic: {}", TOPIC);
+
+            while (running) {
+                Thread.sleep(1000);  // Keep the subscriber alive
+            }
+        } catch (MqttException | InterruptedException e) {
+            dataDestination.alertError("Error in MQTTSubscriber: " + e.getMessage());
+            logger.error("Error in MQTTSubscriber", e);
         }
     }
 
     @Override
     public void connectionLost(Throwable cause) {
-        logger.warn("Connection lost: {}", cause.getMessage());
+        logger.warn("Connection lost", cause);
     }
 
     @Override
     public void messageArrived(String topic, MqttMessage message) {
-        logger.debug("Message arrived. Topic: {} Message: {}", topic, new String(message.getPayload()));
+        String payload = new String(message.getPayload());
+        dataDestination.addSubscriberData(payload);
+        logger.debug("Message arrived. Topic: {} Message: {}", topic, payload);
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
-        // Not used in subscriber
-    }
-
-    public static void main(String[] args) {
-        new MQTTSubscriber();
+        // Not applicable for Subscriber
     }
 }
