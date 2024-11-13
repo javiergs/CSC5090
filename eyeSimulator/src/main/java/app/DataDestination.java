677d8c727c20ac35322712014b8aa2045c15b0fe
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-11-13 14:19:13 -0800

    javadoc comments

--- eyeSimulator/src/main/java/app/DataDestination.java
+++ eyeSimulator/src/main/java/app/DataDestination.java
@@ -1,5 +1,16 @@
 package app;
 
+/**
+ * Interface defining methods for a data destination that receives
+ * subscriber data and handles errors.
+ *
+ * @version 1.2
+ * @authors
+ * Monish Suresh
+ * Christine Widden
+ * Luca Ornstil
+ */
+
 public interface DataDestination {
     void addSubscriberData(String dataWithPrefix);
     void alertError(String messageWithPrefix);
